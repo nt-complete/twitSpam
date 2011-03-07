7ce17b8e75e4fa8cd95f865c91cd9f8ba8b3d79e
nt-cmplt <ntiller@cs.uoregon.edu>
2011-03-07 15:14:55 -0800

    sorted_tweets -> ordered_tweets

--- vowpal_wabbit/combineData.cpp
+++ vowpal_wabbit/combineData.cpp
@@ -2,6 +2,7 @@
 #include <fstream>
 #include <map>
 #include <vector>
+#include <sstream>
 
 int main()
 {
@@ -9,18 +10,22 @@ int main()
   std::ofstream combinedFile;
   std::vector<long> userIds, tweetIds;
   std::string tmpStr;
-  
+  std::sstream strStream;
   std::multimap<long, std::string> userMap, tweetMap;
 
 
   userFile.open("sorted_user_data.txt");
-  tweetFile.open("sorted_tweet_data.txt");
+  tweetFile.open("ordered_tweet_data.txt");
   combinedFile.open("combined_tweet_info.txt");
 
 
   if(userFile.is_open() && tweetFile.is_open() && combinedFile.is_open())
     {
-      
+      while(getline(userFile, tmpStr))
+	{
+	  strStream << tmpStr;
+	  
+	}
 
 
 
