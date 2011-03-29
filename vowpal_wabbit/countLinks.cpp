4a528f4eb562351e8cc0a2380c8604b31d3db615
nt-cmplt <ntiller@cs.uoregon.edu>
2011-03-29 00:38:53 -0700

    countLinks.cpp is completed

--- vowpal_wabbit/countLinks.cpp
+++ vowpal_wabbit/countLinks.cpp
@@ -1,39 +1,77 @@
 #include <map>
 #include <iostream>
 #include <fstream>
+#include <algorithm>
 #include <boost/regex.hpp>
 
-int main()
+int main(int argc, char** argv)
 {
   std::ifstream instream;
   std::string fullStr;
   std::multimap<std::string, int> linkCountMap;
+  std::vector<int> countVec;
+
+  if(argc < 2)
+    {
+      std::cout << "Please input file to count links.\n";
+      return 1;
+    }
+
+  instream.open(argv[1]);
 
 
-  instream.open("retweets_tweets.txt");
   if(instream.is_open())
     {
-      getline(instream, fullStr);
-      
-      boost::regex re("\(http_//[^ ]+)");
-      while(boost::regex_search(fullStr, re))
+      while(getline(instream, fullStr))
 	{
-	  std::string urlStr = boost::regex_replace(fullStr, re, "$1");
-	  std::multimap<std::string, int>::iterator it = linkCountMap.find(urlStr);
 
-	  if(it != linkCountMap.end())
-	    {
-	      (*it).second++;
+      
+	  boost::regex re("^\(.*)\(http_//[^\\s]+)\(.*)$");
+	    while(boost::regex_search(fullStr, re))
+	      {
+		std::string urlStr = boost::regex_replace(fullStr, re, "$2");
+		std::string tmpStr = boost::regex_replace(fullStr, re, "$3");
+		fullStr = boost::regex_replace(fullStr, re, "$1");
+		fullStr += " " + tmpStr;
+		std::multimap<std::string, int>::iterator it = linkCountMap.find(urlStr);
 
-	    }
-	  else
-	    {
-	      linkCountMap.insert(std::pair<std::string, int>(urlStr, 1));
-	    }
+		if(it != linkCountMap.end())
+		  {
+		    (*it).second += 1;
+	
+		  }
+		else
+		  {
+		    linkCountMap.insert(std::pair<std::string, int>(urlStr, 1));
+		  }
+
+	      }
 
 	}
 
+	    for(std::multimap<std::string, int>::iterator it = linkCountMap.begin(); it != linkCountMap.end(); it++)
+	      {
+		countVec.push_back((*it).second);
+	      }
 
+	    sort(countVec.begin(), countVec.end());
+
+	    for(std::vector<int>::iterator it = countVec.begin(); it != countVec.end(); it++)
+	      {
+		std::multimap<std::string, int>::iterator linkIt = linkCountMap.begin();
+		while(linkIt != linkCountMap.end() && (*linkIt).second != *it)
+		  {
+		    linkIt++;
+		  }
+		if(linkIt != linkCountMap.end())
+		  {
+		    if((*linkIt).second > 1)
+		      {
+			std::cout << (*linkIt).second << " - " << (*linkIt).first << "\n";
+		      }
+		    linkCountMap.erase(linkIt);
+		  }
+	      }
 
 
 
