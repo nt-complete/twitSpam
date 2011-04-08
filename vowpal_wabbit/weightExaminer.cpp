c61e61f8f398316a040f6b2f042f2739f098d41a
Your Name <ntiller@cs.uoregon.edu>
2011-04-08 15:36:51 -0700

    weight Examiner works

--- vowpal_wabbit/weightExaminer.cpp
+++ vowpal_wabbit/weightExaminer.cpp
@@ -2,13 +2,15 @@
 #include <fstream>
 #include <map>
 #include <boost/regex.hpp>
+#include <set>
 
 int main(int argc, char** argv)
 {
   std::multimap<double, std::string> tweetWordsMap;
-  
+  std::map<std::string, int> wordCounterMap;
   std::ifstream inputStream;
   std::string fullStr;
+  std::set<std::string> wordSet;
 
   if(argc < 2)
     {
@@ -31,17 +33,29 @@ int main(int argc, char** argv)
 	      std::string valueStr = boost::regex_replace(fullStr, re, "$2");
 	      double value = atof(valueStr.c_str());
 	      fullStr = boost::regex_replace(fullStr, re, "$3");
-	      std::cout << wordStr << " - " << value << "\n";
+	      //std::cout << wordStr << " - " << value << "\n";
 
-	      tweetWordsMap.insert(std::pair<double, std::string>(value, wordStr));
+	      if(wordSet.find(wordStr) == wordSet.end())
+		{
+		  wordSet.insert(wordStr);
+		  tweetWordsMap.insert(std::pair<double, std::string>(value, wordStr));
+		  wordCounterMap.insert(std::pair<std::string, int>(wordStr, 1));
+		}
+	      else
+		{
+		  std::map<std::string, int>::iterator it = wordCounterMap.find(wordStr);
+		  (*it).second++;
+		}
 
 	    }
 
 	}
 
-      for(std::multimap<double, std::string>::iterator it = tweetWordsMap.begin(); it != tweetWordsMap.end(); it++)
+      for(std::multimap<double, std::string >::iterator it = tweetWordsMap.begin(); it != tweetWordsMap.end(); it++)
 	{
-	  std::cout << (*it).second << " - " << (*it).first << "\n";
+	  std::map<std::string, int>::iterator iter = wordCounterMap.find((*it).second);
+	  if((*iter).second > 10)
+	    std::cout << (*it).second << " - " << (*it).first << " - " << (*iter).second << "\n";
 
 	}
 
