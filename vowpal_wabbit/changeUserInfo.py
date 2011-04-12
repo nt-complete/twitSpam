36cfeb980fb1cc4c43292bdd173d2d33aac92256
Your Name <ntiller@cs.uoregon.edu>
2011-04-12 13:41:21 -0700

    adding regex to changeUserInfo.py

--- vowpal_wabbit/changeUserInfo.py
+++ vowpal_wabbit/changeUserInfo.py
@@ -5,7 +5,12 @@ if len(sys.argv) < 2:
 else:
     f = open(sys.argv[1], 'r')
     for line in f:
-        p = re.compile("[a-z]+\d+")
+        """p = re.compile("[a-z]+\d+")
         m = p.search(line)
         if m is not None:
-            print(m.group())
+            print(m.group())"""
+        p = re.match(r"(\|Friends) (\d+)", line)
+        if p is not None:
+            print(p.group(2))
+        else:
+            print (line)
