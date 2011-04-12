3c62145e18b6ae69f059888c65b18d84a509aef7
Your Name <ntiller@cs.uoregon.edu>
2011-04-11 23:31:07 -0700

    updated python

--- vowpal_wabbit/changeUserInfo.py
+++ vowpal_wabbit/changeUserInfo.py
@@ -1,7 +1,11 @@
 import sys
+import re
 if len(sys.argv) < 2:
     print ("Please input file to change.")
 else:
     f = open(sys.argv[1], 'r')
     for line in f:
-        
+        p = re.compile("[a-z]+\d+")
+        m = p.search(line)
+        if m is not None:
+            print(m.group())
